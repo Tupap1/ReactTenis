3778a3840018f13feb0d505a21813fd1bb82ba49
Tupap1 <andresdavidcantillo@gmail.com>
2024-08-16 10:46:27 -0500

    first commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,98 +1,172 @@
 import './Landing.css'
+import aydj from './img/aydj.jpg'
+import nadal from './img/nadal.jpg'
+import serena from './img/serena.webp'
+import federer from './img/federer.jpg'
+import calendario from './img/calendario.png'
 
 const Landing = () => {
     return (
         <div>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-            <div className="container-fluid">
-                <div className="row content">
-                    <div className="col-sm-3 sidenav">
-                        <h4>Tenis Olimpico</h4>
-                        <ul className="nav nav-pills nav-stacked">
-                            <li className="active"><a href="#section1">Home</a></li>
-                            <li><a href="#section2">Friends</a></li>
-                            <li><a href="#section3">Family</a></li>
-                            <li><a href="#section3">Photos</a></li>
-                        </ul><br />
-                        <div className="input-group">
-                            <input type="text" className="form-control" placeholder="Search Blog.." />
-                            <span className="input-group-btn">
-                                <button className="btn btn-default" type="button">
-                                    <span className="glyphicon glyphicon-search"></span>
-                                </button>
-                            </span>
-                        </div>
-                    </div>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+                <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css"/>
+                    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css"/>
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+                        <div className='body' id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
 
-                    <div className="col-sm-9">
-                        <h4><small>RECENT POSTS</small></h4>
-                        <hr />
-                        <h2>I Love Food</h2>
-                        <h5><span className="glyphicon glyphicon-time"></span> Post by Jane Dane, Sep 27, 2015.</h5>
-                        <h5><span className="label label-danger">Food</span> <span className="label label-primary">Ipsum</span></h5><br />
-                        <p>Food is my passion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <br /><br />
 
-                        <h4><small>RECENT POSTS</small></h4>
-                        <hr />
-                        <h2>Officially Blogging</h2>
-                        <h5><span className="glyphicon glyphicon-time"></span> Post by John Doe, Sep 24, 2015.</h5>
-                        <h5><span className="label label-success">Lorem</span></h5><br />
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        <hr />
 
-                        <h4>Leave a Comment:</h4>
-                        <form role="form">
-                            <div className="form-group">
-                                <textarea className="form-control" rows="3" required></textarea>
-                            </div>
-                            <button type="submit" className="btn btn-success">Submit</button>
-                        </form>
-                        <br /><br /><hr /> <br /><br /><br /><br /><br />
 
-                        <p><span className="badge">2</span> Comments:</p><br />
-
-                        <div className="row">
-                            <div className="col-sm-2 text-center">
-                                {/*           <img src="bandmember.jpg" className="img-circle" height="65" width="65" alt="Avatar"> */}
-                            </div>
-                            <div className="col-sm-10">
-                                <h4>Anja <small>Sep 29, 2015, 9:12 PM</small></h4>
-                                <p>Keep up the GREAT work! I am cheering for you!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                <br />
-                            </div>
-                            <div className="col-sm-2 text-center">
-                                {/*           <img src="bird.jpg" className="img-circle" height="65" width="65" alt="Avatar"> */}
-                            </div>
-                            <div className="col-sm-10">
-                                <h4>John Row <small>Sep 25, 2015, 8:25 PM</small></h4>
-                                <p>I am so happy for you man! Finally. I am looking forward to read about your trendy life. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                <br />
-                                <p><span className="badge">1</span> Comment:</p><br />
-                                <div className="row">
-                                    <div className="col-sm-2 text-center">
-                                        {/*               <img src="bird.jpg" className="img-circle" height="65" width="65" alt="Avatar"> */}
+                            <nav className="navbar navbar-default navbar-fixed-top">
+                                <div className="container-fluid">
+                                    <div className="navbar-header">
+                                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                            <span className="icon-bar"></span>
+                                            <span className="icon-bar"></span>
+                                            <span className="icon-bar"></span>
+                                        </button>
+                                        <a className="navbar-brand" href="#myPage">Tenis Olimipico</a>
                                     </div>
-                                    <div className="col-xs-10">
-                                        <h4>Nested Bro <small>Sep 25, 2015, 8:28 PM</small></h4>
-                                        <p>Me too! WOW!</p>
-                                        <br />
+                                    <div className="collapse navbar-collapse" id="myNavbar">
+                                        <ul className="nav navbar-nav navbar-right">
+                                            <li><a href="#myPage">Inicio</a></li>
+                                            <li><a href="#band">Tenis Olímpico</a></li>
+                                            <li><a href="#tour">Tenistas principales</a></li>
+                                            <li><a href="#contact">Paris 2024</a></li>
+                                            <li className="dropdown">
+                                            </li>   
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+
+                            <div id="myCarousel" className="carousel slide" data-ride="carousel">
+
+
+
+                                <div className="carousel-inner" role="listbox">
+                                    <div className="item active">
+                                         <img src={aydj} width="1200" height="700"/>
+                                        <div className="carousel-caption">
+                                            <h3>Alcaraz VS. Djokovic</h3>
+                                            <p>"El duelo de titanes del tenis mundial."</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+
+
+                            </div>
+
+
+                            <div id="band" className="container text-center">
+                                <h3>Tenis Olimpico</h3>
+                                <p><em>We love tennis!</em></p>
+                                <p>"¡Bienvenido al corazón del tenis olímpico! Aquí encontrarás la pasión, la emoción y la adrenalina de los mejores tenistas del mundo compitiendo en el escenario más grande del deporte. Sumérgete en la historia de rivalidades épicas, celebraciones inolvidables y momentos que han marcado la historia del tenis. ¡Vive la magia de los Juegos Olímpicos con nosotros!</p>
+                                <br />
+
+                            </div>
+
+
+                            <div id="tour" className="bg-1">
+                                <div className="container">
+                                    <h3 className="text-center">Tenistas populares</h3>
+                                    <p className="text-center">en esta seccion algunos de los mejores tenistas de todos los tiempos.</p>
+ 
+
+                                    <div className="row text-center">
+                                        <div className="col-sm-4">
+                                            <div className="thumbnail">
+                                                 <img src={nadal} width="400" height="300"/> 
+                                                <p><strong>Rafael Nadal</strong></p>
+                                                <p>El español, conocido por su intensidad y su dominio en tierra batida, ha demostrado ser un verdadero campeón olímpico. Con medallas de oro en individuales (Pekín 2008) y dobles (Río 2016), Nadal ha combinado elegancia y fuerza para conquistar el podio olímpico.</p>
+
+                                            </div>
+                                        </div>
+                                        <div className="col-sm-4">
+                                            <div className="thumbnail">
+                                                <img src={serena} width="400" height="300"/> 
+                                                <p><strong>Serena Williams</strong></p>
+                                                <p>La estadounidense, considerada una de las mejores tenistas de todos los tiempos, ha dominado el tenis femenino durante décadas. Con múltiples medallas de oro olímpicas en individuales y dobles, Serena ha demostrado su superioridad en el escenario más importante del deporte.</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-sm-4">
+                                            <div className="thumbnail">
+                                                <img src={federer}  width="400" height="300"/> 
+                                                <p><strong>Roger Federer</strong></p>
+                                                <p>El suizo, reconocido por su estilo de juego elegante y su palmarés sin igual, ha dejado su huella en el tenis olímpico. Aunque no ha conseguido el oro en individuales, Federer ha logrado la medalla de oro en dobles en los Juegos Olímpicos de Pekín 2008 junto a su compatriota Stan Wawrinka.
+
+</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div className="modal fade" id="myModal" role="dialog">
+                                    <div className="modal-dialog">
+
+
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <button type="button" className="close" data-dismiss="modal">×</button>
+                                                <h4><span className="glyphicon glyphicon-lock"></span> Tickets</h4>
+                                            </div>
+                                            <div className="modal-body">
+                                                <form role="form">
+                                                    <div className="form-group">
+                                                        <label for="psw"><span className="glyphicon glyphicon-shopping-cart"></span> Tickets, $23 per person</label>
+                                                        <input type="number" className="form-control" id="psw" placeholder="How many?" />
+                                                    </div>
+                                                    <div className="form-group">
+                                                        <label for="usrname"><span className="glyphicon glyphicon-user"></span> Send To</label>
+                                                        <input type="text" className="form-control" id="usrname" placeholder="Enter email" />
+                                                    </div>
+                                                    <button type="submit" className="btn btn-block">Pay
+                                                        <span className="glyphicon glyphicon-ok"></span>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                            <div className="modal-footer">
+                                                <button type="submit" className="btn btn-danger btn-default pull-left" data-dismiss="modal">
+                                                    <span className="glyphicon glyphicon-remove"></span> Cancel
+                                                </button>
+                                                <p>Need <a href="#">help?</a></p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
+
+                            <div id="contact" className="container">
+                                <h3 className="text-center">Paris 2024</h3>
+                                <p className="text-center"><em>Men's singles</em></p>
+                                <br />
+
+
+                            <img src={calendario} width="1200" height="800"/>  
+                                <br /><br />
+                                <h3 className="text-center">Gracias por ver!!</h3>
+
+
+                            </div>
+
+
+
+
+                            <footer className="text-center">
+                                <a className="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+                                    <span className="glyphicon glyphicon-chevron-up"></span>
+                                </a><br /><br />
+                                <p>Made by Andres Cantillo Ficha 2669897</p>
+                            </footer>
+
                         </div>
                     </div>
-                </div>
-            </div>
-        <footer class="container-fluid">
-         <p>Footer Text</p>
-        </footer>
-        </div>
-
-
-    )
+                    )
 }
 
-export default Landing
+                    export default Landing
